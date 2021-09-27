8b6b1040d2a669c2d85909629bbc8b36da02a6dc
Nurettin Yilmaz <nyilmaz88@gmail.com>
2021-09-27 16:07:30 +0300

    Make test runnable

--- app/src/test/java/com/migros/McntDemoTest.java
+++ app/src/test/java/com/migros/McntDemoTest.java
@@ -1,31 +1,37 @@
 package com.migros;
 
+import com.migros.app.Application;
+import com.migros.app.TestController;
 import com.migros.generatedapi.api.v2.DefaultApi;
-import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
-@MicronautTest
+@MicronautTest(propertySources = "classpath:application-test.yml", application = Application.class,
+               packages = "com.migros.app")
 class McntDemoTest {
 
-    @Inject
-    EmbeddedServer application;
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
-    @Inject
-    DefaultApi defaultApi;
+  @Inject
+  DefaultApi defaultApi;
 
-    @Test
-    void testItWorks() {
+  @Test
+  void testItWorks() {
 
-        Assertions.assertTrue(application.isRunning());
-
-        Flux<String> productPricing = defaultApi.getProductPricing(12L);
-        String s = productPricing.blockFirst();
-        System.out.println(s);
-    }
-
+    Flux<String> productPricing = defaultApi.getProductPricing(12L);
+    String s = productPricing
+        //.doOnError(e -> {
+        //  if (e instanceof HttpClientResponseException) {
+        //    HttpResponse<?> response = ((HttpClientResponseException) e).getResponse();
+        //    var body = response.getBody(DefaultProblem.class);
+        //    LOGGER.info(body.toString());
+        //  }
+        //})
+        .blockFirst();
+    LOGGER.info(s);
+  }
 }
